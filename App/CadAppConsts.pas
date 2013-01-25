62cfb91629742e5010ca6aaeeb9b41d130bbdd96
Prof1983 <prof1983@yandex.ru>
2013-01-25 10:46:29 +0600

    Updated CadAppConsts

--- App/CadAppConsts.pas
+++ App/CadAppConsts.pas
@@ -2,7 +2,7 @@
 @Abstract Cad.App consts
 @Author Prof1983 <prof1983@ya.ru>
 @Created 26.04.2011
-@LastMod 10.08.2012
+@LastMod 25.01.2013
 }
 unit CadAppConsts;
 
@@ -10,10 +10,68 @@ interface
 
 resourcestring
   ERROR_LOADING = 'Ошибка загрузки документа';
-  ToolMenuBookmarksText = 'Bookmarks';
-  ToolMenuTasksText = 'Tasks';
-  ToolMenuToolsText = 'Tools';
-  ToolMenuViewText = 'View';
+  SMenuBookmarksText = 'Избранное';
+  SMenuFileText = 'Файл';
+  SMenuSprav = 'Справочники';
+  SMenuTasksText = 'Задачи';
+  SMenuToolsText = 'Сервис';
+  SMenuViewText = 'Вид';
+  SMenuVpText = 'Выр.пространство';
+  SMenuWaysText = 'Пути выхода';
+  SBookmarkMenuAdd = 'Добавить в избранное ...';
+  SBookmarkMenuDelete = 'Удалить из избранного ...';
+  SFileMenuImportCooling = 'Импорт всей схемы Cooling';
+  SFileMenuImportCoolingAdd = 'Добавить новые ветви из схемы Cooling';
+  SFileMenuImportDan = 'Импорт из файла .dan';
+  SFileMenuImportFromExcel = 'Импорт факт. расходов из файла Excel';
+  SFileMenuImportFtFromVs = 'Импорт факт. расходов из БД VentSys';
+  SFileMenuNew = 'Новый';
+  SFileMenuOpen = 'Открыть';
+  SFileMenuSave = 'Сохранить';
+  SFileMenuSaveAs = 'Сохранить как...';
+  SSpravMenuVen = 'Вентиляторы';
+  SSpravMenuVu = 'База вент. устройств';
+  STaskMenuCalcWindow = 'Расчет сопротивления окна';
+  STaskMenuDeviceStabilityCur = 'Рассчитать отказ вентиляционного устройства';
+  STaskMenuDeviceStabilitySvod = 'Сводный расчет устойчивости для вент.устр.';
+  STaskMenuEr = 'Нормальное воздухораспределение';
+  STaskMenuGenDan = 'Сгенерировать расчетные данные по рисунку';
+  STaskMenuIr = 'Идеальн.распределение';
+  STaskMenuOr = 'Опт.распределение';
+  STaskMenuEq = 'Проверяет соответствие схемы и расчета';
+  STaskMenuEt = 'Расчет естественной тяги';
+  STaskMenuSvish = 'Стрелки: свежая - исходящая';
+  SToolMenuCalculator = 'Калькулятор';
+  SToolMenuChangeScale = 'Уменьшение или увеличение координат';
+  SToolMenuCutBranch = 'Разбить ветвь';
+  SToolMenuDeleteParasiteBranchs = 'Удалить ветви с отрицательными номерами';
+  SToolMenuDeleteTupik = 'Удаление тупиков';
+  SToolMenuLegendEditor = 'Редактор условных обозначений';
+  SToolMenuPlaSearch = 'Найти позицию ПЛА';
+  SToolMenuRecovery = 'Проверка и исправление схемы';
+  SToolMenuSettings = 'Настройки...';
+  SToolMenuVentOzCur = 'Расчитать очистной забой в выделенной ветви';
+  SViewMenuChangeDataQ = 'Показать/скрыть факт. расход воздуха';
+  SViewMenuData = 'Показать/скрыть расчетные данные';
+  SViewMenuShowAll = 'Показать все объекты';
+  SViewMenuShowNorm = 'Нормальное распределение';
+  SViewMenuShowOtrVetvi = 'Показать ветви с отрицательным расходом';
+  SViewMenuShowPla = 'План ликвидации аварии';
+  SViewMenuShowRas = 'Расчетная схема';
+  SViewMenuShowRReg = 'Ветви с заданным R';
+  SViewMenuShowSchema5 = 'Схема ...';
+  SViewMenuShowVent = 'Схема вентиляции';
+  SWayMenuChoose = 'Выбрать ветвь на схеме';
+  SWayMenuClose = 'Закрыть форму "Маршруты"';
+  SWayMenuCreate = 'Построить маршрут';
+  SWayMenuEdit = 'Редактировать маршрут';
+  SWayMenuList = 'Выбрать ветвь из списка';
+  SWayMenuOpen = 'Перейти к форме "Маршруты"';
+  SWayMenuSave = 'Сохранить маршрут';
+  SWayMenuSaver = 'Маршруты спасателей (ВГСЧ)';
+  SWayMenuTrash = 'Очистить список исключенных ветвей';
+  SWayMenuView = 'Показать маршрут';
+  SWayMenuWorker = 'Маршруты вывода горнорабочих';
 
 implementation
 
